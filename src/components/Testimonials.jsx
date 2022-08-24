3c9b8328fc01d60ef1aa417519f9903a52b1fc09
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-24 16:46:33 +0100

    done with testimonial jsx

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import SectionHead from "./SectionHead"
+import{ImQuotesLeft} from 'react-icons/im'
+import Card from "../UI/Card"
+import {IoIosArrowDropleftCircle, IoIosArrowDroprightCircle} from 'react-icons/io'
+import {testimonials} from '../data'
+
+
+function Testimonials() {
+  const [index, setIndex] = useState(1)
+  const {avatar, name, quote, job} = testimonials[index]
+
+  
+  return (
+    <section className="testimonials">
+      <div className="container testimonials__container">
+        <SectionHead icon={<ImQuotesLeft />} title="Testimonials" />
+        <Card className="testimonial">
+          <div className="testimonial__avatar">
+            <img src={avatar} alt="avatar img" />
+          </div>
+          <p className="testimonial__quote">{`"${quote}"`}</p>
+          <h5>{name}</h5>
+          <small className="testimonial__title">{job}</small>
+        </Card>
+        <div className="testimonials__btn-container">
+          <button className="testimonial__btn"><IoIosArrowDropleftCircle/></button>
+          <button className="testimonial__btn"><IoIosArrowDroprightCircle/></button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Testimonials
