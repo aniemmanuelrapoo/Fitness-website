5201214178040c7135ae297ff9b6c2af11587b8c
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-24 20:52:51 +0100

    header jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,15 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_1.jpg'
 import './about.css'
 
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <Header title="About Us" image={HeaderImage}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic blanditiis labore dolorum consectetur placeat magni esse quam tempore?
+      </Header>
+    </>
   )
 }
 
