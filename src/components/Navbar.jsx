7410b99f408692c966367e5654eeec471aba5e85
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-24 11:33:32 +0100

    done with responsive design for nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
+import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import Logo from '../images/logo.png'
 import { links } from '../data'
 import {GoThreeBars} from 'react-icons/go'
+import {MdOutlineClose} from 'react-icons/md'
 import './navbar.css'
 
 const Navbar = () => {
+  const [isNavShowing, setIsNavShowing] = useState(false);
   return (
     <nav>
       <div className="container nav__container">
         <Link to="/" className='logo'>
           <img src={Logo} alt="Nav Logo" />
         </Link>
-        <ul className='nav__links'>
+        <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav'}`}>
           {
             links.map(({name, path}, index) => {
               return(
@@ -22,8 +25,10 @@ const Navbar = () => {
             })
           }
         </ul>
-        <button className='nav__toggle-btn'>
-          <GoThreeBars />
+        <button className='nav__toggle-btn' onClick={() => setIsNavShowing(!isNavShowing)}>
+          {
+            isNavShowing ? <MdOutlineClose /> : <GoThreeBars />
+          }
         </button>
       </div>
     </nav>
