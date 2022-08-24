ad787d0c53ac8985e934cbf07d4c8d4c1696f377
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-24 09:30:14 +0100

    done with nabar html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
+import { Link, NavLink } from 'react-router-dom'
+import Logo from '../images/logo.png'
+import { links } from '../data'
+import {GoThreeBars} from 'react-icons/go'
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className="container nav__container">
+        <Link to="/" className='logo'>
+          <img src={Logo} alt="Nav Logo" />
+        </Link>
+        <ul className='nav__links'>
+          {
+            links.map(({name, path}, index) => {
+              return(
+                <li>
+                  <NavLink to={path}>{name}</NavLink>
+                </li>
+              )
+            })
+          }
+        </ul>
+        <button className='nav__toggle-btn'>
+          <GoThreeBars />
+        </button>
+      </div>
+    </nav>
   )
 }
 
